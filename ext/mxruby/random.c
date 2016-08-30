209119034d72676aad61df70bec2d1a97c18305d
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2016-08-31 07:29:44 +0900

    4

--- ext/mxruby/random.c
+++ ext/mxruby/random.c
@@ -17,14 +17,14 @@ static VALUE mx_random_alloc(VALUE klass)
 
 static VALUE mx_random_initialize(int argc, VALUE *argv, VALUE self)
 {
-    time_t t;
     VALUE rb_seed;
     MXRANDOM *r;
     Data_Get_Struct(self, MXRANDOM, r);
 
     rb_scan_args(argc, argv, "01", &rb_seed);
-    srand(NIL_P(rb_seed) ? time(&t) : FIX2INT(rb_seed));
+    srand(NIL_P(rb_seed) ? time(NULL) : FIX2INT(rb_seed));
 
+    r->has_value = false;
     r->p = 0;
     for (int i = 0; i < NUM_STATE; i++) {
         r->s[i] = rand();
@@ -33,26 +33,46 @@ static VALUE mx_random_initialize(int argc, VALUE *argv, VALUE self)
     return self;
 }
 
-static uint64_t _xorshift_star_1024(MXRANDOM *r)
+static double _xorshift_star_1024(MXRANDOM *r)
 {
     const uint64_t s0 = r->s[r->p];
     uint64_t s1 = r->s[r->p = (r->p + 1) & 15];
     s1 ^= s1 << 31;
     r->s[r->p] = s1 ^ s0 ^ (s1 >> 11) ^ (s0 >> 30);
-    return r->s[r->p] * UINT64_C(1181783497276652981);
+    return r->s[r->p] * UINT64_C(1181783497276652981) * MIN_DOUBLE;
 }
 
 static VALUE mx_random_rand(VALUE self) {
     MXRANDOM *r;
     Data_Get_Struct(self, MXRANDOM, r);
 
-    double v = _xorshift_star_1024(r) * MIN_DOUBLE;
-    return rb_float_new(v);
+    return rb_float_new(_xorshift_star_1024(r));
 }
 
 static VALUE mx_random_randn(VALUE self)
 {
-    return INT2FIX(2);
+    MXRANDOM *r;
+    Data_Get_Struct(self, MXRANDOM, r);
+
+    double v;
+    if (r->has_value) {
+        v = r->value;
+        r->has_value = false;
+    } else {
+        double x1 = _xorshift_star_1024(r);
+        double x2 = _xorshift_star_1024(r);
+
+        double s = (x1 * x1) + (x2 * x2);
+        double p = sqrt(-2 * log(s) / s);
+        double z1 = x1 * p;
+        double z2 = x2 * p;
+
+        v = z1;
+        r->has_value = true;
+        r->value = z2;
+    }
+
+    return rb_float_new(v);
 }
 
 void Init_random()
--- ext/mxruby/random.h
+++ ext/mxruby/random.h
@@ -1,12 +1,16 @@
 #ifndef _MX_RANDOM_H_
 #define _MX_RANDOM_H_
 
+#include <stdbool.h>
+
 #define NUM_STATE 16
-#define MIN_DOUBLE 5.421010862427522170037264004350e-20
+#define MIN_DOUBLE 5.421010862427522e-20
 
 typedef struct _MXRANDOM {
     uint64_t s[NUM_STATE];
     int p;
+    bool has_value;
+    double value;
 } MXRANDOM;
 
 #endif // _MX_RANDOM_H_
